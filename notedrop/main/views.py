a0e7dc766cb46af9ef861b78a96021ecfc60a1be
Nathan Papes <papes1ns@cmich.edu>
2015-09-04 14:11:22 -0400

    Add functionality for signing up and logging in

--- notedrop/main/views.py
+++ notedrop/main/views.py
@@ -1,33 +1,47 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
-from django.http import HttpResponseNotAllowed
+from django.http import HttpResponse
 from django.contrib.auth.forms import UserCreationForm
 
 from .forms import LoginForm
 
 
 def splash(request, template='splash/splash.html'):
+
+    return render(request, template)
+    
+def feed(request, template='feed/main.html'):
+    return HttpResponse("GOOD Logged in as {0}".format(request.user))
+
+def login_user(request, template='user/login.html'):
+    form = LoginForm()
     if request.method == 'POST':
         form = LoginForm(request.POST)
         if form.is_valid():
             user = authenticate(username=form.cleaned_data['username'],
-                                password=form.cleaned_data['password'])
+                                        password=form.cleaned_data['password'])
             if user is not None:
+                print "USER is NONE"
                 if user.is_active:
                     login(request, user)
                     return redirect('feed')
                 else:
                     error_msg = 'Your account has been deactivated'
-                    return render(request, template, {'login_error': error_msg})
+                    return render(request, template, {
+                        'login_error': error_msg,
+                        'form': form,
+                    })
             else:
                 error_msg = 'Could not find a user with those credentials'
-                return render(request, template, {'login_error': error_msg})
+                return render(request, template, {
+                    'login_error': error_msg,
+                    'form': form,
+                })
         else:
-            return render(request, template, {'login_form': form})
+            return render(request, template, {'form': form})
     
-    form = LoginForm()
-    return render(request, template, {'login_form': form})
-
+    return render(request, template, {'form': form})
+    
             
 def signup(request, template='user/signup.html'):
     if request.method == 'POST':
@@ -42,4 +56,4 @@ def signup(request, template='user/signup.html'):
     return render(request, template, {'form': form })
     
 def enroll(request, template='user/enroll.html'):
-    pass
+    return HttpResponse("Congrats on signing up")
--- notedrop/notedrop/urls.py
+++ notedrop/notedrop/urls.py
@@ -20,11 +20,12 @@ from main import views
 urlpatterns = [
     url(r'^helm/', include(admin.site.urls)),
     
-    url(r'^', views.splash, name='splash'),
+    url(r'^$', views.splash, name='splash'),
     
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^enroll/$', views.enroll, name='enroll'),
+    url(r'^login/$', views.login_user, name='login'),
     
-    url(r'^feed/', views.login, name='feed'),
+    url(r'^feed/', views.feed, name='feed'),
     
 ]
