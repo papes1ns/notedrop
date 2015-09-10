e1bd4d0edd18b2152e733c65248127b8bc42c52a
Nathan Papes <papes1ns@cmich.edu>
2015-09-10 09:49:41 -0400

    Update production and wsgi files

--- notedrop/notedrop/production.py
+++ notedrop/notedrop/production.py
@@ -1,6 +1,12 @@
 from notedrop.settings import *
 
 
+ALLOWED_HOSTS = ['*',]
+
+ADMINS = (
+    ('Nathan Papes', 'papes1ns@cmich.edu'),
+)
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
