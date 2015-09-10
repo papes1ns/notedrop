d856376478bde62496eaf9460737fd3b942c14df
Nathan Papes <papes1ns@cmich.edu>
2015-09-10 16:03:13 -0400

    Changes for server

--- notedrop/production.py
+++ notedrop/production.py
@@ -10,10 +10,12 @@ ADMINS = (
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'notedrop',
-        'USER': 'notedrop',
-        'PASSWORD': 'aFD0#$y`(^Ho|}?T.N'vp9*'0dM?[t`*cm?U@d[wM+:P/ZK].:pM)|\\gp~k(ZV',
+        'NAME': 'ubuntu',
+        'USER': 'ubuntu',
+        'PASSWORD': 'ubuntu',
         'HOST': 'localhost',
         'PORT': '',                      # Set to empty string for default.
     }
 }
+
+STATIC_ROOT = '/webapps/notedrop_env/notedrop/static/'
--- notedrop/settings.py
+++ notedrop/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, "static"),
+    os.path.join(BASE_DIR, "_static"),
 )
 
 LOGIN_URL = '/login/'
