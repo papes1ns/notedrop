a3f5991e6cf2562d1deab121dd7d6bc823621b60
Nathan Papes <you@example.com>
2015-09-11 19:11:42 -0400

    Concise notifyuser MC

--- main/management/commands/notifyusers.py
+++ main/management/commands/notifyusers.py
@@ -15,31 +15,25 @@ class Command(BaseCommand):
         queue = []
 
         for user in UserProfile.objects.all():
-            print user.user.email, user.notify_count
-            if user.user.email:
+            if user.user.email and user.notify_count > 0:
                 recipient = []
-                recipient.append(user.user.email)
-                if user.notify_count > 0:
-                    messages = []
-                    posts = Post.objects.filter(course__in=user.courses.all())
-                    for post in posts:
-                        post_data, created = PostData.objects.get_or_create(post=post, user=user.user)
-                        if post_data.notified is not True:
-                            if user.notify_count <= post.rating:
-                                message = (
-                                    'Post related to {course} received {rating} upvote(s)'.format(course=post.course, rating=post.rating),
-                                    'URL: {host}posts/{postid}/'.format(host=HOSTED_IP, postid=post.pk)
-                                )
-                                message =  ' '.join(message)
-                                messages.append(message)
-                                post_data.notified = True
-                                post_data.save()
+                messages = []
+                for post in Post.objects.filter(course__in=user.courses.all()):
+                    post_data, created = PostData.objects.get_or_create(post=post, user=user.user, notified=False)
+                    if user.notify_count <= post.rating:
+                        message = (
+                            'Post related to {course} received {rating} upvote(s)'.format(course=post.course, rating=post.rating),
+                            'URL: {host}posts/{postid}/'.format(host=HOSTED_IP, postid=post.pk)
+                        )
+                        message =  ' '.join(message)
+                        messages.append(message)
+                        post_data.notified = True
+                        post_data.save()
 
-                    if messages:
-                        message_body = '\n\n'.join(messages)
-                        print messages
-                        email = mail.EmailMessage(MESSAGE_SUBJECT, message_body, settings.EMAIL_HOST_USER, recipient, connection=connection)
-                        queue.append(email)
+                if messages:
+                    message_body = '\n\n'.join(messages)
+                    email = mail.EmailMessage(MESSAGE_SUBJECT, message_body, settings.EMAIL_HOST_USER, recipient, connection=connection)
+                    queue.append(email)
 
 
         connection.send_messages(queue)
